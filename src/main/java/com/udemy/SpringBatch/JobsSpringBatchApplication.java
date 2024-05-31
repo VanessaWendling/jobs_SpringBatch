e2da526af414daf8417ef0014c43248acfafbae5
VanessaWendling <vanessamw2001@hotmail.com>
2024-05-31 12:10:53 -0300

    first commit

---
+++ src/main/java/com/udemy/SpringBatch/JobsSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.SpringBatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsSpringBatchApplication.class, args);
+	}
+
+}
